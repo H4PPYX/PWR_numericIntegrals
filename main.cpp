6b77c9c329d7dd7f0f92851e952d0a2086515d45
Bartek <263731@student.pwr.edu.pl>
2025-04-30 10:15:03 +0200

    test

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 
 double f(double x) {
-    return sin(x);
+    return x*x;
 }
 
 // Metoda prostokątów
 double rectangle_method(double a, double b, int n) {
-    double h = (b - a) / n; // Szerokość przedziału
+    double h = (b - a) / n;
     double sum = 0.0;
 
     for (int i = 0; i < n; i++) {
@@ -21,7 +21,7 @@ double rectangle_method(double a, double b, int n) {
 }
 // Metoda trapezów
 double trapeziod_method(double a, double b, int n) {
-    double h = (b - a) / n; // Szerokość przedziału
+    double h = (b - a) / n;
     double sum = f(a) * f(b) / 2;
 
     for (int i = 0; i < n; i++) {
@@ -31,20 +31,43 @@ double trapeziod_method(double a, double b, int n) {
     return sum * h;
 }
 
-int main() {
-    double a, b;
-    int n;
+// Metoda Simpsona
+double simpson_method(double a, double b, int n) {
+    if (n % 2 != 0) {
+        cout << "Liczba przedziałów n musi być parzysta dla metody Simpsona." << endl;
+    }
 
-    cout << "Podaj przedzial calkowania [a, b]: ";
-    cin >> a >> b;
-    cout << "Podaj liczbe podzialow n: ";
-    cin >> n;
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+
+    /*for (int i = 1; i < n; i++) {*/
+
+        for (int i = 1; i < n; i+=2)
+        {
+            double x1 = a + i * h;
+            for (int y = 2; y < n; y+=2) {
+                double x2 = a + i * h;
+                sum += 4 * x1 + 2 * x2;
+        }
+        }
+
+    return sum * (h / 3);
+}
+
+int main() {
+    double a = 0;
+    double b = 0;
+    int n = 0;
+
+
 
     double result = rectangle_method(a, b, n);
     double result2 = trapeziod_method(a, b, n);
+    //double result3 = trapeziod_method(a, b, n);
 
-    cout << "Przyblizona wartosc calki: " << result << endl;
-    cout << "Przyblizona wartosc calki: " << result2 << endl;
+    cout << "Przyblizona wartosc calki dla metody prostokatow: " << result << endl;
+    cout << "Przyblizona wartosc calki dla metody trapezow: " << result2 << endl;
+    //cout << "Przyblizona wartosc calki dla metody Simpsona: " << result3 << endl;
     return 0;
 }
 
