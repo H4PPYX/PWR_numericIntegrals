a386e454d8a81200a9f1df5abf7e88835a526ca8
Bartek <263731@student.pwr.edu.pl>
2025-05-13 21:24:23 +0200

    sprawdzaine czsu wywolania funkcji

--- main.cpp
+++ main.cpp
@@ -1,55 +1,58 @@
 #include <iostream>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 
 double f(double x) {
-    return /*x*x + x*x*x + 2*x +*/ (cos(x));
+    return 0.2 + 25 * x - 200 * x*x + 675 * x*x*x - 900 * x*x*x*x + 400 * x*x*x*x*x;
 }
 
 // Metoda prostokątów
 double rectangle_method(double a, double b, int n) {
-    double h = (b - a) / n;
+    double h = (b - a) / n; //Obliczanie długości przedziałów
     double sum = 0.0;
 
     for (int i = 0; i < n; i++) {
         double x_i = a + i * h;
-        sum += f(x_i);
+        sum += f(x_i); //Podstawiamy wyznaczone punkty do danej funkcji
     }
-    return sum * h;
+    return sum * h; //Obliczamy sumę pól prostokątów dla poszczególnych przedziałów
 }
 // Metoda trapezów
-double trapeziod_method(double a, double b, int n) {
-    double h = (b - a) / n;
-    double sum = (f(a) + f(b)) / 2.0;
+double trapezoid_method(double a, double b, int n) {
+    double h = (b - a) / n; //Obliczanie długości przedziałów
+    double sum = (f(a) + f(b)) / 2;
 
     for (int i = 0; i < n; i++) {
         double x_i = a + i * h;
-        sum += f(x_i);
+        sum += f(x_i); //Podstawiamy wyznaczone punkty do danej funkcji
     }
-    return sum * h;
+    return sum * h; //Obliczamy sumę pól trapezów dla poszczególnych przedziałów
 }
 
 // Metoda Simpsona
 double simpson_method(double a, double b, int n) {
     if (n % 2 != 0) {
-        cout << "Liczba przedziałów n musi być parzysta dla metody Simpsona." << endl;
+        cout << "Liczba przedzialow n musi byc parzysta dla metody Simpsona." << endl;
+        return 0;
     }
 
-    double h = (b - a) / n;
+    double h = (b - a) / n; //Obliczanie długości przedziałów
     double sum = f(a) + f(b);
 
     for (int i = 1; i < n; i += 2)
     {
         double x = a + i * h;
-        sum += 4 * f(x);
+        sum += 4 * f(x); //Suma nieparzystych
     }
 
     for (int i = 2; i < n; i += 2)
     {
         double x = a + i * h;
-        sum += 2 * f(x);
+        sum += 2 * f(x); //Suma parzystych
     }
 
     return sum * (h / 3) ;
@@ -60,7 +63,7 @@ double gauss_chebyshev(int n) {
     double sum = 0.0;
 
     for (int i = 1; i <= n; i++) {
-        double x_i = cos(M_PI * (2.0 * i - 1) / (2.0 * n));
+        double x_i = cos(M_PI * (2.0 * i - 1) / (2.0 * n)); //Generowanie miejsc zerowych wielomianu Czebyszewa
         sum += f(x_i) * sqrt(1 - x_i * x_i);
     }
 
@@ -70,19 +73,36 @@ double gauss_chebyshev(int n) {
 int main() {
     double a = -1;
     double b = 1;
-    int n = 200;
+    int n = 2;
 
 
+    auto start1 = high_resolution_clock::now();
+    double r = rectangle_method(a, b, n);
+    auto stop1 = high_resolution_clock::now();
+    auto d1 = duration_cast<nanoseconds>(stop1 - start1);
+    cout << "Przyblizona wartosc calki dla metody prostokatow: " << r << ", czas = " << d1.count() << " ns\n";
 
-    double result = rectangle_method(a, b, n);
-    double result2 = trapeziod_method(a, b, n);
-    double result3 = simpson_method(a, b, n);
-    double result4 = gauss_chebyshev(n);
 
-    cout << "Przyblizona wartosc calki dla metody prostokatow: " << result << endl;
-    cout << "Przyblizona wartosc calki dla metody trapezow: " << result2 << endl;
-    cout << "Przyblizona wartosc calki dla metody Simpsona: " << result3 << endl;
-    cout << "Przyblizona wartosc calki dla kwadratury Gaussa-Czebyszewa: " << result4 << endl;
+    auto start2 = high_resolution_clock::now();
+    double t = trapezoid_method(a, b, n);
+    auto stop2 = high_resolution_clock::now();
+    auto d2 = duration_cast<nanoseconds>(stop2 - start2);
+    cout << "Przyblizona wartosc calki dla metody trapezow: = " << t << ", czas = " << d2.count() << " ns\n";
+
+
+    auto start3 = high_resolution_clock::now();
+    double s = simpson_method(a, b, n);
+    auto stop3 = high_resolution_clock::now();
+    auto d3 = duration_cast<nanoseconds>(stop3 - start3);
+    cout << "Przyblizona wartosc calki dla metody Simpsona: " << s << ", czas = " << d3.count() << " ns\n";
+
+
+    auto start4 = high_resolution_clock::now();
+    double g = gauss_chebyshev(n);
+    auto stop4 = high_resolution_clock::now();
+    auto d4 = duration_cast<nanoseconds>(stop4 - start4);
+    cout << "Przyblizona wartosc calki dla kwadratury Gaussa-Czebyszewa: = " << g << ", czas = " << d4.count() << " ns\n";
+
     return 0;
 }
 
